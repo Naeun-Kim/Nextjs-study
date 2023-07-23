b6d052bdbb2561fc46504d87646275a25e9ffdc2
Naeun-Kim <naeun@Naeunui-MacBookPro.local>
2023-07-23 16:06:31 +0900

    api 설정

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { getProduct, getProducts } from '@/service/product';
 import { notFound } from 'next/navigation'
 
 type Props = {
@@ -12,16 +13,18 @@ export function generateMetadata({params}: Props) {
     }
 }
 
-export default function Pants({params}: Props) {
-    if (params.slug === 'nothing') {
+export default async function ProductPage({params: {slug}}: Props) {
+    const product = await getProduct(slug);
+    if (!product) {
         notFound();
     }
-    return  <h1>{params.slug} 제품 설명 페이지</h1>
+    return  <h1>{product.name} 설명 페이지</h1>
 }
 
-export function generateStaticParams() {
-    const products = ['pants', 'skirt'];
+export async function generateStaticParams() {
+    //모든 제품의 페이지들을 미리 만들어 두기(SSG)
+    const products = await getProducts();
     return products.map(product => ({
-        slug: product
-    }))
+        slug: product.id,
+    }));
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,18 +1,25 @@
+import MeowArticle from "@/components/MeowArticle";
+import { getProducts } from "@/service/product";
 import Link from "next/link";
 
-const products = ['shirt', 'pants', 'skirt', 'shoes'];
-
-export default function Products() {
+export default async function Products() {
+    const products = await getProducts();
+    const res = await fetch('https://meowfacts.herokuapp.com', {
+        next: { revalidate: 3},
+    });
+    const data = await res.json();
+    const factText = data.data[0];
     return (
         <>
             <h1>제품 소개 페이지</h1>
             <ul>
-                {products.map((product, index) => (
+                {products.map(({ id, name }, index) => (
                     <li key={index}>
-                        <Link href={`products/${product}`}>{product}</Link>
+                        <Link href={`products/${id}`}>{name}</Link>
                     </li>
                 ))}
             </ul>
+            <MeowArticle />
         </>
     )
 }
